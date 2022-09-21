b375b2fc5118ca218570bab86f1bfd11ebbdf439
LunaRossie <>
2022-09-21 01:19:02 -0400

    Add to files

--- script.js
+++ script.js
@@ -14,3 +14,51 @@ var timerCount;
 
 var lettersInChosenWord = [];
 var blanksLetters = [];
+
+var words = ["variable","array", "modulus", "object", "function", "string", "boolean"];
+
+function init() {
+    getWins();
+    getlosses();
+  }
+  function startGame() {
+    isWin = false;
+    timerCount = 10;
+    // Prevents start button from being clicked when round is in progress
+    startButton.disabled = true;
+    renderBlanks()
+    startTimer()
+  }
+  function winGame() {
+    wordBlank.textContent = "You Won!";
+    winCounter++
+  startButton.disabled = false;
+  setWins()
+}
+function loseGame() {
+    wordBlank.textContent = "GAME OVER";
+    loseCounter++
+    startButton.disabled = false;
+    setLosses()
+  }
+  function startTimer() {
+    // Sets timer
+    timer = setInterval(function() {
+      timerCount--;
+      timerElement.textContent = timerCount;
+      if (timerCount >= 0) {
+        // Tests if win condition is met
+        if (isWin && timerCount > 0) {
+          // Clears interval and stops timer
+          clearInterval(timer);
+          winGame();
+        }
+      }
+      // Tests if time has run out
+      if (timerCount === 0) {
+        // Clears interval
+        clearInterval(timer);
+        loseGame();
+      }
+    }, 1000);
+  }
